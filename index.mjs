337b126142b9a3ee05ad31207036d765691ffa9d
RehmanKhan007 <156161992+RehmanKhan007@users.noreply.github.com>
2025-01-03 09:16:04 +0500

    cors error

--- index.mjs
+++ index.mjs
@@ -1,10 +1,22 @@
 import express from 'express'
+import { products } from './productData.mjs'
+import cors from 'cors'
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Afaq Khan Niazi')
+app.use(cors())
+
+app.get('/', (req, response) => {  
+  response.send(`Hello World`)
 })
+
+app.get('/product', (request, response) => {  
+   console.log('request',request);
+
+  // response.send({name:'Mango',price:200,quantity:'2kg'})
+  response.send(products)
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
