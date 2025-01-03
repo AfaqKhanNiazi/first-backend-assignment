82e66c484e2eaf0a1152166829cfe4a88057b0e1
RehmanKhan007 <156161992+RehmanKhan007@users.noreply.github.com>
2025-01-03 10:11:52 +0500

    node modules

--- index.mjs
+++ index.mjs
@@ -2,9 +2,9 @@ import express from 'express'
 import { products } from './productData.mjs'
 import cors from 'cors'
 const app = express()
-const port = 3000
+const port =process.env.PORT || 3000
 
-app.use(cors())
+app.use(cors({origin:""}))
 
 app.get('/', (req, response) => {  
   response.send(`Hello World`)
